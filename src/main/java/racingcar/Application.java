9a8ec8780d52022314b2a3ca2d59d9bf9bb35a7f
eengeop <leeeus99@gmail.com>
2025-10-27 14:42:49 +0900

    feat: Controller를 통해 프로그램 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.controller.AppController;
+import racingcar.controller.RacingController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingController racingCarController = AppController.run();
+        racingCarController.start();
     }
 }
