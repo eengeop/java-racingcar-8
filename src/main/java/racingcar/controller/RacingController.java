9de844de3817c187ce0513bc0bdb9dee198b6ba1
eengeop <leeeus99@gmail.com>
2025-10-27 14:39:22 +0900

    feat: 생성자 주입, 입력 값 검증, 프로그램 실행 담당

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,56 +1,30 @@
 package racingcar.controller;
 
-import racingcar.domain.UserInputDTO;
-import racingcar.domain.RacingCar;
 import racingcar.service.RacingService;
 import racingcar.validation.Validator;
+import racingcar.view.InputView;
 
 import java.util.List;
 
 public class RacingController {
 
-    private Validator validator = new Validator();
-    private RacingService racingService = new RacingService();
+    private final Validator validator;
+    private final RacingService racingService;
 
-    public List<RacingCar> validUserInput(UserInputDTO userInputDTO){
-        List<String> carNames = validator.validateName(userInputDTO);
-        String tryCount = validator.validateCount(userInputDTO);
-        return racingService.startRacing(carNames);
+    public RacingController(Validator validator, RacingService racingService){
+        this.validator = validator;
+        this.racingService = racingService;
     }
 
-    public void racingResultDTO(List<RacingCar> racingCar, String tryCount){
-
+    private List<String> validInputName(String carNames){
+        return validator.validateName(carNames);
     }
 
-
-    public void run(){
-        /*
-        * aaa = InputView.inputNameAndCount()
-        * bbb = controller.validUserInput(aaa)
-        * ccc = racingResultDTO(bbb, Integer.parseInt(aaa.getTryCount()))
-        * */
+    private int validInputTryCount(String tryCount){
+        return validator.validateCount(tryCount);
     }
 
-
-
-/*    public static void validateName(RacingCar racingCar){
-        String[] CarNameArr = racingCar.carName().split(",");
-        for(String arr : CarNameArr){
-            if(arr.length()>5){
-                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
-            }
-        }
+    public void start(){
+        racingService.startRacing(validInputName(InputView.inputName()), validInputTryCount(InputView.inputTryCount()));
     }
-
-    public static void validateCount(RacingCar racingCar){
-        try{
-            int tryCount = Integer.parseInt(racingCar.tryCount());
-            if(tryCount>10){
-                throw new IllegalArgumentException("시도는 최대 10회만 가능합니다.");
-            }
-        }
-        catch (NumberFormatException e){
-            throw new IllegalArgumentException();
-        }
-    }*/
 }
